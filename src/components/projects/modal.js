96f2f61dc19292aef3455a74fe2c8dddd2c9f6e9
peter <leepeter019@gmail.com>
2020-06-14 21:40:08 -0700

    redesigned project modal

--- src/components/projects/modal.js
+++ src/components/projects/modal.js
@@ -5,41 +5,44 @@ function Modal(props) {
         <div class="modal fade modal-container" id={`${props.title}-modal`} tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                 <div className="modal-content">
-                    <div className="modal-container">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <div className="project-features">
-                            <h2>Features...</h2>
-                            <div className="row">
-                                {props.features.map(item => (<div className="col-md feature-container"> 
-                                    <div className="feature">
-                                        <i className={item.icon}></i> 
-                                        <div>{item.feature}</div>
-                                    </div>
-                                </div>))}
-                            </div>
+                  <div className="project-links">
+                    <a href={props.links[1]} target="_blank" className="project-link">
+                        <i class="fab fa-github"></i><span>&nbsp;Repo</span>
+                    </a>
+                    <a href={props.links[0]} target="_blank" className="project-link">
+                        <i class="fas fa-play"></i><span>&nbsp;Live</span>
+                    </a>
+                  </div>
+                  <div className="row project-summary">
+                    This is the project summary
+                  </div>
+                  <div className="modal-container row">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                      <div className="project-features col-6">
+                          <h2>Features...</h2>
+                          <div>
+                              {props.features.map(item => (<div className="col-md feature-container"> 
+                                  <div className="feature">
+                                      <i className={item.icon}></i> 
+                                      <div className="feature-desc">{item.feature}</div>
+                                  </div>
+                              </div>))}
+                          </div>
+                      </div>
+                      <hr />
+                      <div className="project-tech col-6">
+                        <h2>Created Using...</h2>
+                        <div className="tech">
+                            {props.tech.map(item => (
+                                <div className="tech-item">
+                                    <img src={window.location.origin + "/images/" + item + ".png"} />
+                                </div>
+                            ))}
                         </div>
-                        <hr />
-                        <div className="project-tech">
-                            <h2>Created Using..</h2>
-                            <div className="tech">
-                                {props.tech.map(item => (
-                                    <div className="tech-item">
-                                        <img src={window.location.origin + "/images/" + item + ".png"} />
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="project-links">
-                            <a href={props.links[1]} target="_blank" className="project-link">
-                                <i class="fab fa-github"></i> &nbsp;Repo
-                            </a>
-                            <a href={props.links[0]} target="_blank" className="project-link">
-                                <i class="fas fa-play"></i>&nbsp;Live
-                            </a>
-                        </div>
-                    </div>
+                      </div>
+                  </div>
                 </div>
             </div>
         </div>
