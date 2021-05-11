670d46b0755bbc50da2f3ea644d248196d68bb94
peter <leepeter019@gmail.com>
2021-05-10 21:06:50 -0700

    Changed to no capital letters. Only icons animate on hover.

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,19 +8,19 @@ function Footer() {
         <a className={styles.icon} href={window.location.origin + "/Resume.pdf"} target="_blank" rel="noopener noreferrer" >
           <i class="far fa-file"></i>
         </a> 
-        <div className={styles.words}>RESUME</div>
+        <div className={styles.words}>Resume</div>
       </div>
       <div className={styles.item}>
         <a className={styles.icon} href="mailto: leepeter019@gmail.com">
           <i class="far fa-envelope"></i>
         </a> 
-        <div className={styles.words}>E-MAIL</div>
+        <div className={styles.words}>E-mail</div>
       </div>
       <div className={styles.item}>
         <a className={styles.icon} href="https://github.com/PedroL33" target="_blank" rel="noopener noreferrer" >
           <i class="fab fa-github"></i>
         </a> 
-        <div className={styles.words}>GITHUB</div>
+        <div className={styles.words}>Github</div>
       </div>
     </div>
   )
